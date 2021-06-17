ada2af12b7e30770973343689572a37ef60e4ed9
真島　祐志 <dat.gp1219@gmail.com>
2021-06-17 09:57:23 +0900

    Initial commit

---
+++ v0617/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace v0617
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int vx = -10;
+            int vy = -10;
+            label1.Left = vx;
+            label1.Top = vy;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
